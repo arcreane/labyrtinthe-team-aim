788447aabb74147c839a151bbea5eff8c05fe7f9
ilan <ilan.petiot@icloud.com>
2021-12-14 18:26:35 +0100

    ✨- Creation of project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.coding.labyrtintheteamaim {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.coding.labyrtintheteamaim to javafx.fxml;
+    exports com.coding.labyrtintheteamaim;
+}
