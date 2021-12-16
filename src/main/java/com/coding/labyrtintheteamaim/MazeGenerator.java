7a62b1765d0896cbc27f19748446da6a50b68d08
ilan <ilan.petiot@icloud.com>
2021-12-16 15:52:39 +0100

    ✏️- Write and print solution for maze

--- src/main/java/com/coding/labyrtintheteamaim/MazeGenerator.java
+++ src/main/java/com/coding/labyrtintheteamaim/MazeGenerator.java
@@ -1,9 +1,12 @@
 package com.coding.labyrtintheteamaim;
 
+import java.io.*;
 import java.util.Collections;
 import java.util.Arrays;
+import com.coding.labyrtintheteamaim.MazeSolver;
 
 import static com.coding.labyrtintheteamaim.MazeConfigurator.start;
+import static com.coding.labyrtintheteamaim.MazeSolver.*;
 
 //recursive backtracking algorithm
 
@@ -17,26 +20,36 @@ public class MazeGenerator {
         this.y = height;
         maze = new int[this.x][this.y];
         generateMaze(0, 0);
+
     }
 
-    public void display() {
+    public void display() throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Solution.txt", false));
+
         for (int i = 0; i < y; i++) {
             // draw the north edge
             for (int j =0; j < x; j++) {
                 System.out.print((maze[j][i] & 1) == 0 ? "+---" : "+   ");
+                writer.write((maze[j][i] & 1) == 0 ? "+---" : "+   ");
             }
             System.out.println("+");
+            writer.write("+\n");
             // draw the west edge
             for (int j = 0; j < x; j++) {
                 System.out.print((maze[j][i] & 8) == 0 ? "|   " : "    ");
+                writer.write((maze[j][i] & 8) == 0 ? "|   " : "    ");
             }
             System.out.println("|");
+            writer.write("|\n");
         }
         // draw the bottom line
-        for (int j = 0; j < x -1; j++) { //x -1 to add the maze exit
+        for (int j = 0; j < x; j++) {
             System.out.print("+---");
+            writer.write("+---");
         }
         System.out.println("+");
+        writer.write("+\n");
+        writer.flush();
     }
 
     //
@@ -47,11 +60,8 @@ public class MazeGenerator {
             int nx = cx + dir.dx;
             int ny = cy + dir.dy;
             if (between(nx, x) && between(ny, y) && (maze[nx][ny] == 0)) {
-                if (cx == 0){
-                    maze[cx][cy] |= dir.bit-1 ; // to add the maze entrance
-                }else{
-                    maze[cx][cy] |= dir.bit;
-                }
+
+                maze[cx][cy] |= dir.bit ; // to add the maze entrance
                 maze[nx][ny] |= dir.opposite.bit;
                 generateMaze(nx, ny);
             }
@@ -84,9 +94,16 @@ public class MazeGenerator {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main (String[] args) throws IOException
+    {
         start();
-//        MazeGenerator maze = new MazeGenerator(x, y);
-//        maze.display();
+        InputStream f = new FileInputStream("Solution.txt");
+        String[] lines = readLines (f);
+        char[][] maze = decimateHorizontally (lines);
+        solveMaze (maze);
+        String[] solvedLines = expandHorizontally (maze);
+        for (int i = 0  ;  i < solvedLines.length  ;  i++)
+            System.out.println (solvedLines[i]);
     }
+
 }
